faaad5c4d4d4863fa3e72e6b506ffb931d78cd4c
Christian <christiannmenezes@gmail.com>
2024-07-21 12:27:59 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+$names = ["João", "Maria"];
+
+var_dump($names);
+
+$names[] = "Márcia";
+$names[] = "Adriana";
+$names[] = "Celeste";
+$names[] = "Cristina";
+
+var_dump($names);
+
+$names[] = "Vanessa";
+$names[] = "Vivian";
+$names[] = "Maria José";
+
+var_dump($names);
